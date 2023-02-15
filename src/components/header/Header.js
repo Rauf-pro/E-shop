867f022de59f21ae61967066c89c6089462680ca
rrauf8178 <rrauf8178@gmail.com>
2023-02-16 03:22:43 +0400

    Created Header component and styled

---
+++ src/components/header/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+
+const logo = (
+  <div className={styles.logo}>
+    <Link to="/">
+      <h2>
+        e<span>Shop</span>.
+      </h2>
+    </Link>
+  </div>
+);
+
+const Header = () => {
+  return (
+    <header>
+      <div className={styles.header}>
+        {logo}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className={styles["header-right"]}>
+            <span className={styles.links}>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/order-history">My Orders</Link>
+            </span>
+            <span className={styles.cart}>
+                <Link to="/cart">Cart</Link>
+            </span>
+          </div>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
